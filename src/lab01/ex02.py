4a260f3f8f8cf7637f4546ee79ac04eb046e76a2
insignema <akindtim@gmail.com>
2025-09-23 09:48:05 +0300

    second commit

---
+++ src/lab01/ex02.py
@@ -0,0 +1,3 @@
+a = float(input('A: ').replace(',', '.'))
+b = float(input('B: ').replace(',', '.'))
+print(f'sum={a+b:.2f}; avg={(a+b)/2:.2f}')
---
+++ src/lab01/ex04.py
@@ -0,0 +1,2 @@
+time = int(input("Минуты: "))
+print(f'{(time//60):02d}:{(time%60):02d}')
